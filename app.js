b31b2f7100143edba3e65664b35ee8310be0b63c
Shlok Upadhyay <50832393+shloku8@users.noreply.github.com>
2022-07-25 19:37:44 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World! This is the Node App!');
-});
-app.listen(8080, function () {
-});
